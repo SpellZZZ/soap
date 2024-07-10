ba90b27046c48faf8caacff94ad13603789e7c15
SpellZZZ <james321321@wp.pl>
2024-07-10 21:05:56 +0200

    Add endpoint fix reactive

---
+++ src/main/java/com/example/expensemanagersoap/endpoint/ExpenseEndpoint.java
@@ -0,0 +1,40 @@
+package com.example.expensemanagersoap.endpoint;
+
+import com.example.expensemanagersoap.repo.ExpenseRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import soap.demo.web_service_test.Expense;
+import soap.demo.web_service_test.GetExpenseRequest;
+import soap.demo.web_service_test.GetExpenseResponse;
+
+import java.util.Optional;
+
+@Endpoint
+public class ExpenseEndpoint {
+    private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+    final private ExpenseRepo expenseRepo;
+
+    @Autowired
+    public ExpenseEndpoint(ExpenseRepo expenseRepo) {
+        this.expenseRepo = expenseRepo;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "getExpenseRequest")
+    @ResponsePayload
+    public GetExpenseResponse getExpense(@RequestPayload GetExpenseRequest request) {
+        GetExpenseResponse response = new GetExpenseResponse();
+        Optional<Expense> optionalExpense = expenseRepo.findById(request.getId());
+
+        if (optionalExpense.isPresent()) {
+            response.setExpense(optionalExpense.get());
+        } else {
+            response = null;
+        }
+
+        return response;
+    }
+}
--- src/main/java/com/example/expensemanagersoap/repo/ExpenseRepo.java
+++ src/main/java/com/example/expensemanagersoap/repo/ExpenseRepo.java
@@ -1,11 +1,11 @@
 package com.example.expensemanagersoap.repo;
 
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 import soap.demo.web_service_test.Expense;
 
 @Repository
-public interface ExpenseRepo extends ReactiveMongoRepository<Expense,String> {
+public interface ExpenseRepo extends MongoRepository<Expense,Long> {
 
 
 
