ac7fada2ea4d5559fc945eebf5e6fb049dd60550
SpellZZZ <james321321@wp.pl>
2024-07-10 20:44:46 +0200

    ADD repo fix pom

---
+++ src/main/java/com/example/expensemanagersoap/repo/ExpenseRepo.java
@@ -0,0 +1,11 @@
+package com.example.expensemanagersoap.repo;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+import soap.demo.web_service_test.Expense;
+
+@Repository
+public interface ExpenseRepo extends ReactiveMongoRepository<Expense,String> {
+
+
+}
