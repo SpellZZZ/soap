bb36b1c98fdaa3acb6fd86ce52b6cfebbf98358e
SpellZZZ <james321321@wp.pl>
2024-07-10 20:28:29 +0200

    Init project

---
+++ src/main/java/com/example/expensemanagersoap/ExpenseManagerSoapApplication.java
@@ -0,0 +1,13 @@
+package com.example.expensemanagersoap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseManagerSoapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExpenseManagerSoapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/expensemanagersoap/ExpenseManagerSoapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.expensemanagersoap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseManagerSoapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
